ba0c9c08aac42919a70de51c131a6416b7ed8839
Estudante <>
2020-01-21 21:49:02 -0300

    no message

--- bank/src/main/java/com/br/qualiti/bank/controller/CustomerController.java
+++ bank/src/main/java/com/br/qualiti/bank/controller/CustomerController.java
@@ -1,10 +1,20 @@
 package com.br.qualiti.bank.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.qualiti.bank.exception.ResourceNotFoundException;
 import com.br.qualiti.bank.model.Customer;
 import com.br.qualiti.bank.service.CustomerService;
 
@@ -16,8 +26,40 @@ public class CustomerController {
 	CustomerController(CustomerService customerService) {
 		this.customerService = customerService;
 	}
-@PostMapping
-public Customer create(@RequestBody Customer customer) {
-	return customerService.create(customer);
-}
+
+	@PostMapping
+	public Customer create(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+
+	@GetMapping
+	public List<Customer> findAll() {
+		return customerService.findAll();
+	}
+
+	@GetMapping(path = { "/{id}" })
+	public ResponseEntity findById(@PathVariable long id) {
+		Optional<Customer> customer = customerService.findById(id);
+		if (customer.isPresent()) {
+			return ResponseEntity.ok().body(customer);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity update(@PathVariable("id") long id, @RequestBody Customer customer) {
+		try {
+			Customer updatedCustomer = customerService.update(id, customer);
+			return ResponseEntity.ok().body(updatedCustomer);
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping(path = { "/{id}" })
+	public ResponseEntity delete(@PathVariable long id) {
+		customerService.delete(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
--- bank/src/main/java/com/br/qualiti/bank/service/CustomerService.java
+++ bank/src/main/java/com/br/qualiti/bank/service/CustomerService.java
@@ -1,7 +1,12 @@
 package com.br.qualiti.bank.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.br.qualiti.bank.exception.ResourceNotFoundException;
 import com.br.qualiti.bank.model.Customer;
 import com.br.qualiti.bank.repository.CustomerRepository;
 
@@ -15,5 +20,34 @@ public class CustomerService {
 
 	public Customer create(Customer customer) {
 		return customerRepository.save(customer);
+	}
+
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+
+	}
+
+	public Optional<Customer> findById(long id) {
+		return customerRepository.findById(id);
+	}
+
+	public Customer update(long id, Customer customer) {
+		Optional<Customer> currentCustomer = customerRepository.findById(id);
+		if (currentCustomer.isPresent()) {
+			currentCustomer.get().setName(customer.getName());
+			currentCustomer.get().setCPF(customer.getCPF());
+			currentCustomer.get().getAccounts().addAll(customer.getAccounts());
+			return customerRepository.save(currentCustomer.get());
+		} else {
+			throw new ResourceNotFoundException("Customer", "Client", "O cliente com id:" + id + " nao foi encontrado");
+		}
+	}
+    
+	public void delete(long id) {
+		try {
+			customerRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+
+		}
 	}
 }
